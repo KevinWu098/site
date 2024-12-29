fb5e006fba2c4b10cd4a1de5d7934259e6421742
Kevin Wu <kevinwu098@gmail.com>
2024-12-29 05:14:42 -0800

    feat: write markdown

--- src/app/consume/(consume-mdx-layout)/layout.tsx
+++ src/app/consume/(consume-mdx-layout)/layout.tsx
@@ -1,6 +1,17 @@
+import { cn } from "@/lib/utils";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="mx-auto flex w-full flex-col space-y-8 pt-16">
+        <div
+            className={cn(
+                "flex w-full max-w-full flex-col pt-16",
+                "prose text-custom-text font-sans",
+                "prose-headings:text-custom-heading",
+                "prose-h1:text-custom-primary prose-h1:font-semibold prose-h1:font-playfair-display prose-h1:text-3xl",
+                "prose-a:text-custom-muted prose-a:font-normal",
+                "prose-p:text-custom-muted"
+            )}
+        >
             {children}
         </div>
     );
--- src/components/consume/consume-item.tsx
+++ src/components/consume/consume-item.tsx
@@ -12,17 +12,22 @@ export function ConsumeItem({
     title,
     category,
     date: _date,
-    link,
 }: ConsumeItemProps) {
     const date = new Date(_date).toLocaleDateString("en-US", {
         month: "short",
         year: "numeric",
     });
 
+    const href = `${title
+        .toLowerCase()
+        .replace(/[^a-z0-9 ]/g, "")
+        .split(/\s+/)
+        .join("-")}`;
+
     return (
-        <div className="border-custom-muted/20 border-l-2 pl-2">
+        <div className="border-custom-muted/20 hover:border-custom-primary/50 border-l-2 pl-4 transition duration-300">
             <Link
-                href={`${title.toLowerCase().replaceAll(" ", "-")}`}
+                href={href}
                 className="no-underline"
             >
                 <div className="flex flex-row items-center space-x-1">
@@ -32,19 +37,6 @@ export function ConsumeItem({
                     <p className="text-custom-muted capitalize">{category}</p>
                     <DotIcon className="stroke-custom-muted" />
                     <p className="text-custom-muted">{date}</p>
-                    {/* {link ? (
-                        <>
-                            <DotIcon className="stroke-custom-muted" />
-                            <Link
-                                href={link}
-                                target="_blank"
-                                referrerPolicy="no-referrer"
-                                className="group inline-flex no-underline"
-                            >
-                                <p className="text-custom-muted">Link</p>
-                            </Link>
-                        </>
-                    ) : null} */}
                 </div>
             </Link>
         </div>
