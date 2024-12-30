633c4887c27b814e5f99259bcc966d3c8cc7bae5
Kevin Wu <kevinwu098@gmail.com>
2024-12-30 05:19:09 -0800

    feat: links to subdomains

--- src/components/links/links.tsx
+++ src/components/links/links.tsx
@@ -1,15 +1,28 @@
+import Link from "next/link";
 import { LinkIcon } from "@/components/links/link";
 import { LINKS_DATA } from "@/components/links/links-data";
+import { DotIcon } from "lucide-react";
 
 export function Links() {
     return (
-        <div className="flex flex-row space-x-4">
-            {LINKS_DATA.map((item) => (
-                <LinkIcon
-                    iconLink={item}
-                    key={item.href}
-                />
-            ))}
+        <div className="flex flex-row items-center space-x-2">
+            <div className="flex flex-row items-center space-x-4">
+                {LINKS_DATA.map((item) => (
+                    <LinkIcon
+                        iconLink={item}
+                        key={item.href}
+                    />
+                ))}
+            </div>
+
+            {/* <DotIcon className="size-5" />
+
+            <Link
+                href={"/consume"}
+                className="leading-none no-underline"
+            >
+                consume
+            </Link> */}
         </div>
     );
 }
