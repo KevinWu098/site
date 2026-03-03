46b35fcaa6f23c7a7527e1b577f61d5bf2fc07e3
Kevin Wu <kevinwu098@gmail.com>
2026-03-02 22:24:16 -0600

    chore: update data

--- src/components/create/create-data.ts
+++ src/components/create/create-data.ts
@@ -7,6 +7,18 @@ export const CREATE_DATA: CreateItemProps[] = [
         link: "https://github.com/kevinwu098/meme-vault",
         date: "November 2025",
     },
+    {
+        title: "Radioactive Duck Game",
+        category: "projects",
+        link: "https://github.com/laurelin60/radioactive-duck-game",
+        date: "November 2025",
+    },
+    {
+        title: "Agentic",
+        category: "projects",
+        link: "https://github.com/laurelin60/agentic",
+        date: "January 2024",
+    },
 ].sort((a, b) => {
     const dateA = new Date(a.date);
     const dateB = new Date(b.date);
